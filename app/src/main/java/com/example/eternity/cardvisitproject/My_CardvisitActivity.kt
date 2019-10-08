5e1be179047da5a0b3a6de2874b7568bb50d9a0c
nagashima <1709018@st.asojuku.ac.jp>
2019-10-08 14:31:05 +0900

    my_card作ったよ

---
+++ app/src/main/java/com/example/eternity/cardvisitproject/My_CardvisitActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eternity.cardvisitproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class My_CardvisitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my__cardvisit)
+    }
+}
