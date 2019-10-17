f460f49c56dd202517e3758dc683c099bf5f323f
チャンニャットタム <trannhattam.aso@gmail.com>
2019-10-17 15:22:29 +0900

    GPS権限

--- app/src/main/java/com/example/eternity/cardvisitproject/Card_ChangeActivity.kt
+++ app/src/main/java/com/example/eternity/cardvisitproject/Card_ChangeActivity.kt
@@ -1,12 +1,39 @@
 package com.example.eternity.cardvisitproject
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationProvider
 import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 
 class Card_ChangeActivity : AppCompatActivity() {
+
+    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card__change)
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1000)
+        } else {
+
+
+        }
     }
+
+
 }
