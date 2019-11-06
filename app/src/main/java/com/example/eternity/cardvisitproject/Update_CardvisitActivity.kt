4f7d69656a2d257b0a5419d7ed2cfc0f7f11e34f
nagashima <1709018@st.asojuku.ac.jp>
2019-11-06 15:21:35 +0900

    spinner形だけ作ったよ11/6

--- app/src/main/java/com/example/eternity/cardvisitproject/Update_CardvisitActivity.kt
+++ app/src/main/java/com/example/eternity/cardvisitproject/Update_CardvisitActivity.kt
@@ -2,11 +2,32 @@ package com.example.eternity.cardvisitproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_update__cardvisit.*
 
 class Update_CardvisitActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update__cardvisit)
+
+        Color_sp.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override
+                fun onItemSelected(parent: AdapterView<*>?,
+                                   view: View?,
+                                   position: Int,
+                                   id: Long){
+                    val spinner = parent as? Spinner
+                    val item = spinner?.selectedItem as? String
+                    item?.let{
+                        if(it.isNotEmpty()) Companyname.text = it
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
     }
 }
