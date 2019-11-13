2863cd23b10eca9318944b61171418bd8c47fe32
nagashima <1709018@st.asojuku.ac.jp>
2019-11-13 11:38:07 +0900

    色変更作成中11/13

--- app/src/main/java/com/example/eternity/cardvisitproject/Update_CardvisitActivity.kt
+++ app/src/main/java/com/example/eternity/cardvisitproject/Update_CardvisitActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_information__register.*
 import kotlinx.android.synthetic.main.activity_update__cardvisit.*
 
 class Update_CardvisitActivity : AppCompatActivity() {
@@ -16,18 +17,32 @@ class Update_CardvisitActivity : AppCompatActivity() {
         Color_sp.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
                 override
-                fun onItemSelected(parent: AdapterView<*>?,
-                                   view: View?,
-                                   position: Int,
-                                   id: Long){
+                fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
                     val spinner = parent as? Spinner
                     val item = spinner?.selectedItem as? String
-                    item?.let{
-                        if(it.isNotEmpty()) Companyname.text = it
+                    item?.let {
+                        if (it.isNotEmpty()){
+                            //Companyname.textColors = it
+                        }
                     }
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
+
+        Fontsize_bt.setOnClickListener({
+            Companyname.setTextSize(80f)
+        })
+
+        /*Companyname.setTextIsSelectable(boolean selectable){
+            mTextIsSelectable = true;
+        }*/
+
+
     }
 }
