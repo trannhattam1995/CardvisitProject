ce54d836def2c24f283a35602f9a28e3f81de538
チャンニャットタム <trannhattam.aso@gmail.com>
2019-11-26 11:38:21 +0900

    テンプレートを追加

--- app/src/main/java/com/example/eternity/cardvisitproject/DrawView.kt
+++ app/src/main/java/com/example/eternity/cardvisitproject/DrawView.kt
@@ -1,8 +1,6 @@
 package com.example.eternity.cardvisitproject
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -11,16 +9,14 @@ class DrawView : View {
     var paint : Paint = android.graphics.Paint()
     var path : Path = Path()
 
-
+    var myTextViews  : ArrayList<MyTextView> = ArrayList()
 
     constructor(context: Context) : super(context) {
         path = Path()
 
         paint = Paint()
         paint.color = -0xff7800
-        paint.style = Paint.Style.STROKE
-        paint.strokeJoin = Paint.Join.ROUND
-        paint.strokeCap = Paint.Cap.ROUND
+
         paint.setStrokeWidth(10f)
     }
 
@@ -29,18 +25,25 @@ class DrawView : View {
 
         paint = Paint()
         paint.color = -0xff7800
-        paint.style = Paint.Style.STROKE
-        paint.strokeJoin = Paint.Join.ROUND
-        paint.strokeCap = Paint.Cap.ROUND
-        paint.setStrokeWidth(10f)
+
+        paint.setStrokeWidth(30f)
     }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawPath(path, paint)
+        var bitmap : Bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources , R.drawable.temple1) , this.width,this.height, false )
+        canvas.drawBitmap(bitmap , 0f , 0f , paint)
 
+        myTextViews.add(MyTextView(50f,50f,Color.RED , "test"))
+        for(textview in myTextViews){
+            paint.color = textview.color
+            paint.textSize = textview.fontSize
+            canvas.drawText( textview.content , textview.x , textview.y ,paint)
+        }
+
+        ///canvas.dra
 
     }
 
@@ -50,7 +53,7 @@ class DrawView : View {
 
         when (event.getAction()) {
             MotionEvent.ACTION_DOWN -> {
-                path.moveTo(x, y)
+
                 invalidate()
             }
             MotionEvent.ACTION_MOVE -> {
@@ -59,6 +62,7 @@ class DrawView : View {
             }
             MotionEvent.ACTION_UP -> {
                 path.lineTo(x, y)
+
                 invalidate()
             }
         }
